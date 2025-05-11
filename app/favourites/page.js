be181990ff8561d4a1381f6b674ef31ec7ab0934
Meer Hamza <meerhamza6813@gmail.com>
2025-05-11 16:17:25 +0500

    Initial commit

--- app/favourites/page.js
+++ app/favourites/page.js
@@ -1,10 +1,54 @@
+// "use client"
+
+// import React, { useState } from "react";
+// import "./favourites.css";
+
+// const Favourites = () => {
+//   const [isGridView, setIsGridView] = useState(true);
+//   const favouriteItems = [
+//     { id: 1, name: "Khaadi", image: "/brands/khaadi.jpg", cashback: "60%" },
+//     { id: 2, name: "Cheezious", image: "/brands/cheezious.png", cashback: "60%" },
+//     { id: 3, name: "Sapphire", image: "/brands/sapphire.png", cashback: "60%" },
+//     { id: 4, name: "Rivaj", image: "/brands/rivaj.jpeg", cashback: "60%" },
+//   ];
+
+//   return (
+//     <div className="favourites-container">
+//       {/* Header */}
+//       <div className="favourites-header">
+//         <h2>❤️ Your Favourites</h2>
+//         <button onClick={() => setIsGridView(!isGridView)}>
+//           {isGridView ? "List View" : "Grid View"}
+//         </button>
+//       </div>
+
+//       {/* Favourites List */}
+//       <div className={isGridView ? "grid-view" : "list-view"}>
+//         {favouriteItems.map((item) => (
+//           <div key={item.id} className="fav-card">
+//             <img src={`${item.image}`} alt={item.name} />
+//             <div className="fav-info">
+//               <h3>{item.name}</h3>
+//               {/* <p>{item.cashback} Cashback</p> */}
+//               <button className="remove-btn">Remove</button>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Favourites;
+
+
+
 "use client"
 
-import React, { useState } from "react";
+import React from "react";
 import "./favourites.css";
 
 const Favourites = () => {
-  const [isGridView, setIsGridView] = useState(true);
   const favouriteItems = [
     { id: 1, name: "Khaadi", image: "/brands/khaadi.jpg", cashback: "60%" },
     { id: 2, name: "Cheezious", image: "/brands/cheezious.png", cashback: "60%" },
@@ -14,22 +58,16 @@ const Favourites = () => {
 
   return (
     <div className="favourites-container">
-      {/* Header */}
       <div className="favourites-header">
         <h2>❤️ Your Favourites</h2>
-        <button onClick={() => setIsGridView(!isGridView)}>
-          {isGridView ? "List View" : "Grid View"}
-        </button>
       </div>
 
-      {/* Favourites List */}
-      <div className={isGridView ? "grid-view" : "list-view"}>
+      <div className="list-view">
         {favouriteItems.map((item) => (
           <div key={item.id} className="fav-card">
             <img src={`${item.image}`} alt={item.name} />
             <div className="fav-info">
               <h3>{item.name}</h3>
-              {/* <p>{item.cashback} Cashback</p> */}
               <button className="remove-btn">Remove</button>
             </div>
           </div>
